2921697a1025aa7bdca21e2985e4132d06bf8c92
Andy Mwirigi <akmwirigi23@gmail.com>
2025-12-09 12:03:40 +0300

    Adding functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 function gameObject() {
     return {
         home: {
@@ -114,3 +115,178 @@ function gameObject() {
         },
     };
 }
+
+
+function numPointsScored(playerName) {
+  let game = gameObject()
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      if (name === playerName) {
+        return players[name].points
+      }
+    }
+  }
+  return 0
+}
+
+function shoeSize(playerName) {
+  let game = gameObject()
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      if (name === playerName) {
+        return players[name].shoe
+      }
+    }
+  }
+  return 0
+}
+
+function teamColors(teamName) {
+  let game = gameObject()
+  
+  for (let team in game) {
+    if (game[team].teamName === teamName) {
+      return game[team].colors
+    }
+  }
+  return []
+}
+
+function teamNames() {
+  let game = gameObject()
+  let names = []
+  
+  for (let team in game) {
+    names.push(game[team].teamName)
+  }
+  return names
+}
+
+function playerNumbers(teamName) {
+  let game = gameObject()
+  let numbers = []
+  
+  for (let team in game) {
+    if (game[team].teamName === teamName) {
+      let players = game[team].players
+      for (let playerName in players) {
+        numbers.push(players[playerName].number)
+      }
+    }
+  }
+  return numbers
+}
+
+function playerStats(playerName) {
+  let game = gameObject()
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      if (name === playerName) {
+        return players[name]
+      }
+    }
+  }
+  return {}
+}
+
+function bigShoeRebounds() {
+  let game = gameObject()
+  let biggestShoe = 0
+  let rebounds = 0
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let playerName in players) {
+      let player = players[playerName]
+      if (player.shoe > biggestShoe) {
+        biggestShoe = player.shoe
+        rebounds = player.rebounds
+      }
+    }
+  }
+  return rebounds
+}
+
+function mostPointsScored() {
+  let game = gameObject()
+  let mostPoints = 0
+  let playerName = ""
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      let player = players[name]
+      if (player.points > mostPoints) {
+        mostPoints = player.points
+        playerName = name
+      }
+    }
+  }
+  return playerName
+}
+
+function winningTeam() {
+  let game = gameObject()
+  let homePoints = 0
+  let awayPoints = 0
+ 
+  let homePlayers = game.home.players
+  for (let playerName in homePlayers) {
+    homePoints += homePlayers[playerName].points
+  }
+  
+  
+  let awayPlayers = game.away.players
+  for (let playerName in awayPlayers) {
+    awayPoints += awayPlayers[playerName].points
+  }
+  
+  if (homePoints > awayPoints) {
+    return game.home.teamName
+  } else {
+    return game.away.teamName
+  }
+}
+
+function playerWithLongestName() {
+  let game = gameObject()
+  let longestName = ""
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      if (name.length > longestName.length) {
+        longestName = name
+      }
+    }
+  }
+  return longestName
+}
+
+function doesLongNameStealATon() {
+  let longestNamePlayer = playerWithLongestName()
+  let game = gameObject()
+  let mostSteals = 0
+  let stealsPlayerName = ""
+  
+  for (let team in game) {
+    let players = game[team].players
+    for (let name in players) {
+      let player = players[name]
+      if (player.steals > mostSteals) {
+        mostSteals = player.steals
+        stealsPlayerName = name
+      }
+    }
+  }
+  
+  return longestNamePlayer === stealsPlayerName
+}
+
+ 
